22ae5cf1e77fa57c7eae1f71fc5cea1ae33714be
Shreyas Kaundinya <shreyassk08@gmail.com>
2022-11-01 02:11:29 +0530

    Font, basic loading

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,44 @@
+import { useState, useEffect } from 'react';
 import '../styles/globals.css';
+import Router from 'next/router';
+import Loading from '../src/components/Loading'
 
 function MyApp({ Component, pageProps }) {
-    return <Component {...pageProps} />;
+    const [loading, setLoading] = useState(1);
+
+    useEffect(() => {
+        let timeout;
+        // HANDLE LOADING ---------------
+        const handleLoading = () => {
+            setLoading(1);
+        };
+        const handleLoaded = () => {
+            timeout = setTimeout(() => {
+                setLoading(() => 0);
+            }, 1000);
+        };
+        const loadingTimeout = setTimeout(() => {
+            setLoading(() => 0);
+        }, 2000);
+
+        if (process.browser) {
+            Router.events.on('routeChangeStart', handleLoading);
+            Router.events.on('routeChangeComplete', handleLoaded);
+        }
+
+        return () => {
+            Router.events.off('routeChangeStart', handleLoading);
+            Router.events.off('routeChangeComplete', handleLoaded);
+            clearTimeout(timeout);
+            clearTimeout(loadingTimeout);
+        };
+    }, []);
+
+    return loading ? (
+        <Loading loading={loading} />
+    ) : (
+        <Component {...pageProps} />
+    );
 }
 
 export default MyApp;
