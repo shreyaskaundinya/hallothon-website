2e531fb85ea53c75b5557bc4dd52d3a9f9e76d11
ExpectoX1 <79239242+ExpectoX1@users.noreply.github.com>
2022-11-01 22:03:33 +0530

    ps

--- pages/register/index.js
+++ pages/register/index.js
@@ -11,8 +11,8 @@ function Registration() {
                 <link rel='icon' href='/favicon.ico' />
             </Head>
             <Navbar />
-            <div className='text-5xl text-center py-16'>Registrations Opening at 11:59 PM Today</div>
-            {/* <RegistrationForm /> */}
+            {/* <div className='text-5xl text-center py-16'>Registrations Opening at 11:59 PM Today</div> */}
+             <RegistrationForm />
         </div>
     );
 }
