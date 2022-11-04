371095c6441f7ae5133ae7563181ef372d7df4d7
Shreyas Kaundinya <shreyassk08@gmail.com>
2022-11-04 22:08:59 +0530

    Updated loading state

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,9 @@
-import { useState, useEffect } from 'react';
-import '../styles/globals.css';
 import Router from 'next/router';
-import Loading from '../src/components/Loading'
+import { useEffect, useState } from 'react';
+import Loading from '../src/components/Loading';
+import '../styles/globals.css';
+
+import { animated, config, Transition } from 'react-spring';
 
 function MyApp({ Component, pageProps }) {
     const [loading, setLoading] = useState(1);
@@ -34,10 +36,26 @@ function MyApp({ Component, pageProps }) {
         };
     }, []);
 
-    return loading ? (
-        <Loading loading={loading} />
-    ) : (
-        <Component {...pageProps} />
+    return (
+        <Transition
+            items={loading}
+            from={{ opacity: 0 }}
+            enter={{ opacity: 1 }}
+            reverse={loading}
+            delay={200}
+            config={config.molasses}>
+            {(styles, item) => {
+                return loading ? (
+                    <animated.div style={styles}>
+                        <Loading loading={loading} />
+                    </animated.div>
+                ) : (
+                    <animated.div style={styles}>
+                        <Component {...pageProps} />
+                    </animated.div>
+                );
+            }}
+        </Transition>
     );
 }
 
