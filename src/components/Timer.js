a276514a48bdc13fba3a8d9d940fb6d0ade0885d
Shreyas Kaundinya <shreyassk08@gmail.com>
2022-11-09 01:55:46 +0530

    Added done state to timer

--- src/components/Timer.js
+++ src/components/Timer.js
@@ -1,6 +1,8 @@
 import { useEffect, useState } from 'react';
 
 function Timer(props) {
+    const [timeup, setTimeup] = useState(false);
+
     const calculateTimeLeft = () => {
         const difference = new Date(props.date) - new Date();
         let timeLeft = [];
@@ -12,9 +14,13 @@ function Timer(props) {
                 Math.floor((difference / 1000 / 60) % 60),
                 Math.floor((difference / 1000) % 60),
             ];
+            return timeLeft;
+        } else {
+            setTimeup(() => true);
+            if (props.setDone) {
+                props.setDone(() => true);
+            }
         }
-
-        return timeLeft;
     };
 
     const [timeLeft, setTimeLeft] = useState([0, 0, 0, 0]);
@@ -33,42 +39,56 @@ function Timer(props) {
 
     return (
         <div className='bg-black timer-container flex justify-evenly items-center h-24 sm:h-24 overflow-hidden'>
-            <div>
-                <span className='text-step-0 md:text-step-4 pr-2'>
-                    {timeLeft[0] < 10 ? '0' + timeLeft[0] : timeLeft[0]}
-                </span>
-                <span className='text-step-2'>
-                    <span className='hidden lg:visible lg:inline'>Days</span>
-                    <span className='visible lg:hidden'>D</span>
-                </span>
-            </div>
-            <div>
-                <span className='text-step-0 md:text-step-4 pr-2'>
-                    {timeLeft[1] < 10 ? '0' + timeLeft[1] : timeLeft[1]}
-                </span>
-                <span className='text-step-2'>
-                    <span className='hidden lg:visible lg:inline'>Hours</span>
-                    <span className='visible lg:hidden'>H</span>
-                </span>
-            </div>
-            <div>
-                <span className='text-step-0 md:text-step-4 pr-2'>
-                    {timeLeft[2] < 10 ? '0' + timeLeft[2] : timeLeft[2]}
-                </span>
-                <span className='text-step-2'>
-                    <span className='hidden lg:visible lg:inline'>Minutes</span>
-                    <span className='visible lg:hidden'>M</span>
-                </span>
-            </div>
-            <div>
-                <span className='text-step-0 md:text-step-4 pr-2'>
-                    {timeLeft[3] < 10 ? '0' + timeLeft[3] : timeLeft[3]}
-                </span>
-                <span className='text-step-2'>
-                    <span className='hidden lg:visible lg:inline'>Seconds</span>
-                    <span className='visible lg:hidden'>S</span>
-                </span>
-            </div>
+            {timeup ? (
+                <p>Time is up!</p>
+            ) : (
+                <>
+                    <div>
+                        <span className='text-step-0 md:text-step-4 pr-2'>
+                            {timeLeft[0] < 10 ? '0' + timeLeft[0] : timeLeft[0]}
+                        </span>
+                        <span className='text-step-2'>
+                            <span className='hidden lg:visible lg:inline'>
+                                Days
+                            </span>
+                            <span className='visible lg:hidden'>D</span>
+                        </span>
+                    </div>
+                    <div>
+                        <span className='text-step-0 md:text-step-4 pr-2'>
+                            {timeLeft[1] < 10 ? '0' + timeLeft[1] : timeLeft[1]}
+                        </span>
+                        <span className='text-step-2'>
+                            <span className='hidden lg:visible lg:inline'>
+                                Hours
+                            </span>
+                            <span className='visible lg:hidden'>H</span>
+                        </span>
+                    </div>
+                    <div>
+                        <span className='text-step-0 md:text-step-4 pr-2'>
+                            {timeLeft[2] < 10 ? '0' + timeLeft[2] : timeLeft[2]}
+                        </span>
+                        <span className='text-step-2'>
+                            <span className='hidden lg:visible lg:inline'>
+                                Minutes
+                            </span>
+                            <span className='visible lg:hidden'>M</span>
+                        </span>
+                    </div>
+                    <div>
+                        <span className='text-step-0 md:text-step-4 pr-2'>
+                            {timeLeft[3] < 10 ? '0' + timeLeft[3] : timeLeft[3]}
+                        </span>
+                        <span className='text-step-2'>
+                            <span className='hidden lg:visible lg:inline'>
+                                Seconds
+                            </span>
+                            <span className='visible lg:hidden'>S</span>
+                        </span>
+                    </div>
+                </>
+            )}
         </div>
     );
 }
