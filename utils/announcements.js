84893a8a40d2f9cc615f6259c44af7cd5af67028
Ankush <anshhv2003@gmail.com>
2023-10-04 06:01:57 +0000

    Update 3 files

--- utils/announcements.js
+++ utils/announcements.js
@@ -1,9 +1,5 @@
 export const announcements = [
     {
-        title: 'WIFI Credentials',
-        desc: 'Credential 1: CSEHE01 (username and password same); Credential 2: CSEHE02 (username and password same); Credential 3: CSEHE03 (username and password same);',
-    },
-    {
         title: 'Discord Server',
         desc: 'Link : https://discord.com/invite/YwCdT9xC',
     },
